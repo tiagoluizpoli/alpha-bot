53f6e8d645713eedf65632dabe2d4a0aebabcdf9
Tiago Poli <tiago.poli@stoneage.com.br>
2024-01-10 00:05:44 -0300

    refactor: backing things up

--- src/bot/commands/create-draw.ts
+++ src/bot/commands/create-draw.ts
@@ -106,6 +106,7 @@ export class CreateDrawCommand implements ICommandBuilder {
               }
             },
           ],
+          ['leave-button', (buttonInteraction) => {}],
         ]),
       }),
     );
